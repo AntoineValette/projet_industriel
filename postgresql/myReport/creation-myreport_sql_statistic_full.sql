5fc725cd7b70ac48cf1018fe3abd6811c4d51aec
Antoine Valette <[email protected]antoine.valette.73@gmail.comgit config --global user.name Your>
2025-01-15 14:36:56 +0100

    modif tables VARCHAR devient REAL

--- postgresql/myReport/creation-myreport_sql_statistic_full.sql
+++ postgresql/myReport/creation-myreport_sql_statistic_full.sql
@@ -11,15 +11,15 @@ CREATE TABLE IF NOT EXISTS myreport_sql_statistic_full
 (
 	ident BIGSERIAL,
     date_heure VARCHAR(255),
-    date_heure_raw VARCHAR(20),
-    nombre_requetes_lots VARCHAR(35),
-    nombre_requetes_lots_raw VARCHAR(40),
-    compilations_sql VARCHAR(25),
-    compilations_sql_raw VARCHAR(30),
-    recompilations_sql VARCHAR(30),
-    recompilations_sql_raw VARCHAR(35),
-    temps_mort_pct VARCHAR(20),
-    temps_mort_raw VARCHAR(20),
-    couverture_pct VARCHAR(20),
-    couverture_raw VARCHAR(20)
+    date_heure_raw REAL,
+    nombre_requetes_lots REAL,
+    nombre_requetes_lots_raw REAL,
+    compilations_sql REAL,
+    compilations_sql_raw REAL,
+    recompilations_sql REAL,
+    recompilations_sql_raw REAL,
+    temps_mort_pct REAL,
+    temps_mort_raw REAL,
+    couverture_pct REAL,
+    couverture_raw REAL
 );
