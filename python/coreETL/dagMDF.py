85d53ed65ce197904d02d366ff12fa3dde9b02ae
manon <manon.kobsch@student-cs.fr>
2025-02-12 11:22:09 +0100

    ajout d'une nouvelle branche feature pour automatisation par script python

---
+++ python/coreETL/dagMDF.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# name : dagMDF.py
+# author :
+# version :
+# desc : python script to transform raw data to a time serie data frame
+
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+if __name__ == "__main__":
+    print("debut du script", time.ctime())
