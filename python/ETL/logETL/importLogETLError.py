e7d1e61f9a28b82a2c2cd6034bdfc7b483643ebf
manon <manon.kobsch@student-cs.fr>
2025-02-19 22:43:24 +0100

    feature notebook to script - modification des open/closes - on n'ouvre la connexion que  si le fichier csv à importer existe

--- python/ETL/logETL/importLogETLError.py
+++ python/ETL/logETL/importLogETLError.py
@@ -6,13 +6,13 @@ from core.coreLog import log
 from core.settings import Settings
 
 def importLogETLerror():
-    log("Connexion à PostgreSQL")
-    conn = psycopg2.connect(Settings.POSTGRES_URL)
-    cur = conn.cursor()
-
-    log("import de 241016_LogETLError")
     filename = "/data/logETL/241016_LogETLError.csv"
     if os.path.isfile(filename):
+        log("Connexion à PostgreSQL")
+        conn = psycopg2.connect(Settings.POSTGRES_URL)
+        cur = conn.cursor()
+
+        log("import de 241016_LogETLError")
         with open(filename, 'r', encoding='utf-8-sig') as f:
             reader = csv.DictReader(f, delimiter=';', quoting=csv.QUOTE_MINIMAL)
             next(reader)
@@ -20,8 +20,10 @@ def importLogETLerror():
                 cur.execute("INSERT INTO logERR (server_version, client_version, product, project_name, model, log_date, log_time, row_num, log_type, log_message, etl_start_datetime, launcher_id, launcher_name, machine, program_id, program_name, schedules_id, schedules_name, schedules_start_datetime) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", tuple(row.values()))
         log("import de 241016_LogETLError [ok]")
         conn.commit()
+        cur.close()
 
-    cur.close()
-    conn.close()
-    log("fermeture de la connexion PostgreSQL")
+        log("transform 241016_LogETLError ...")
+        log("load 241016_LogETLError ...")
 
+        conn.close()
+        log("PostgreSQL - close")
